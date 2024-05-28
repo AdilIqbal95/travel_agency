c4bb9608825a500c60622c9724a0af984bcfca77
adiliqbal95 <adil.iqbal@hotmail.co.uk>
2024-05-28 15:03:51 +0100

    Added remaining tests for BedAndBreakfast

--- src/test/java/BedAndBreakfastTest.java
+++ src/test/java/BedAndBreakfastTest.java
@@ -23,5 +23,66 @@ public class BedAndBreakfastTest {
         assertThat(bedAndBreakfast.getCustomer()).isEqualTo(expectedResult);
     }
 
+    @Test
+    public void canBook__customerCantAfford() {
+        bedAndBreakfast.book(customer2);
+        assertThat(bedAndBreakfast.getCustomer()).isEqualTo(null);
+    }
+
+    @Test
+    public void canBook__notEnoughCapacity() {
+        bedAndBreakfast.book(customer);
+        bedAndBreakfast.book(customer2);
+        assertThat(bedAndBreakfast.getCustomer()).isEqualTo(customer);
+    }
+
+
+    @Test
+    public void canCancel() {
+        bedAndBreakfast.cancel(customer);
+        assertThat(bedAndBreakfast.getCustomer()).isEqualTo(null);
+    }
+
+    @Test
+    public void checkCapacity__true() {
+        assertThat(bedAndBreakfast.hasCapacity()).isEqualTo(true);
+    }
+
+    @Test
+    public void checkCapacity__false() {
+        bedAndBreakfast.book(customer);
+        assertThat(bedAndBreakfast.hasCapacity()).isEqualTo(false);
+    }
+
+    @Test
+    public void canGetPrice() {
+        assertThat(bedAndBreakfast.getPrice()).isEqualTo(20);
+    }
+
+    @Test
+    public void canSetPrice() {
+        bedAndBreakfast.setPrice(30);
+        assertThat(bedAndBreakfast.getPrice()).isEqualTo(30);
+    }
+
+    @Test
+    public void canGetCustomer__whenEmpty() {
+        assertThat(bedAndBreakfast.getCustomer()).isEqualTo(null);
+    }
+
+    @Test
+    public void canGetCustomer__whenNotEmpty() {
+        bedAndBreakfast.book(customer);
+        assertThat(bedAndBreakfast.getCustomer()).isEqualTo(customer);
+    }
+
+    @Test
+    public void canSetCustomer() {
+        Customer newCustomer = new Customer("Charlie", 40);
+        bedAndBreakfast.setCustomer(newCustomer);
+        assertThat(bedAndBreakfast.getCustomer()).isEqualTo(newCustomer);
+    }
+
+
 
 }
